f2a4fe14177c47caf32510d3df26d576d4d6d4fa
CIFU1199 <edwkamilo@gmail.com>
2022-07-22 20:12:54 -0500

    Mostrando un Spinner de carga

--- js/app.js
+++ js/app.js
@@ -54,8 +54,9 @@ function consultarAPI(ciudad ,pais){
     const appID = '56f13bbd5293af42eda51c5d6e9d4532';
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`
-
-    fetch(url)
+    Spinner();// muestra un spinner de carga
+    setTimeout(() => {
+        fetch(url)
         .then( respuesta => respuesta.json())
         .then(datos => {
             limpiarHTML();
@@ -66,7 +67,9 @@ function consultarAPI(ciudad ,pais){
             
             //imprime la respuesta en el html 
             mostrarClima(datos);
-        }) 
+        })
+    }, 700);
+     
 }
 
 function mostrarClima(datos){
@@ -76,7 +79,7 @@ function mostrarClima(datos){
     const CMax = kelvinACentigrados(temp_max);
     const CMin = kelvinACentigrados(temp_min);
     const NombreCiudad = document.createElement('p');
-    NombreCiudad.textContent(`Clima en: ${name}`);
+    NombreCiudad.textContent = `Clima en: ${name}`;
     NombreCiudad.classList.add('font-bold','text-6xl');
 
 
@@ -110,4 +113,27 @@ function limpiarHTML() {
     while (resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
+}
+
+function Spinner (){
+    limpiarHTML();
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML=`
+    <div class="sk-circle1 sk-circle"></div>
+    <div class="sk-circle2 sk-circle"></div>
+    <div class="sk-circle3 sk-circle"></div>
+    <div class="sk-circle4 sk-circle"></div>
+    <div class="sk-circle5 sk-circle"></div>
+    <div class="sk-circle6 sk-circle"></div>
+    <div class="sk-circle7 sk-circle"></div>
+    <div class="sk-circle8 sk-circle"></div>
+    <div class="sk-circle9 sk-circle"></div>
+    <div class="sk-circle10 sk-circle"></div>
+    <div class="sk-circle11 sk-circle"></div>
+    <div class="sk-circle12 sk-circle"></div>
+    `;
+
+    resultado.appendChild(divSpinner);
 }
