153823319ed211b7a398d7017745ed93b3aadd7c
CIFU1199 <edwkamilo@gmail.com>
2022-07-22 19:06:01 -0500

    Obtener una API key

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ window.addEventListener('load', ()=>{
 function buscarClima (e) {
     e.preventDefault();
 
-
     // Validar 
     const ciudad = document.querySelector('#ciudad').value;
     const pais = document.querySelector('#pais').value;   
@@ -22,13 +21,6 @@ function buscarClima (e) {
     consultarAPI(ciudad, pais);
 }
 
-function consultarAPI(ciudad ,pais){
-    const appID = '56f13bbd5293af42eda51c5d6e9d4532';
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`
-
-    console.log(url);
-}
 
 function mostrarError(mensaje){
     
@@ -57,3 +49,19 @@ function mostrarError(mensaje){
    
 }
 
+
+function consultarAPI(ciudad ,pais){
+    const appID = '56f13bbd5293af42eda51c5d6e9d4532';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`
+
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then(datos => {
+            console.log(datos);
+            if(datos.cod === "404"){
+                console.log(datos);
+                mostrarError('Ciudad no encontrada');
+            }
+        }) 
+}
